76ac6a721f46a5cf84f7aff1dce95eb5f6a7f07c
rei <67372053+reiugit@users.noreply.github.com>
2024-06-26 09:58:35 +0200

    Projektdateien hinzufügen.

---
+++ AppSettingsInConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false);
+
+var configuration = builder.Build();
+
+Console.WriteLine();
+
+Console.WriteLine($"NumericSetting: {configuration.GetValue<int>("NumericSetting")}\n");
+
+Console.WriteLine($"TextSettingA: {configuration["SettingsGroup:SettingA"]}");
+Console.WriteLine($"TextSettingB: {configuration["SettingsGroup:SettingB"]}");
+
+
