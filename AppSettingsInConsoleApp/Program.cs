1695b503470a83a42ec4613c3eadc5e78e987f1d
rei <67372053+reiugit@users.noreply.github.com>
2024-06-26 10:36:31 +0200

    Also including a "secrets.json" file, if this file exist on the users computer.

--- AppSettingsInConsoleApp/Program.cs
+++ AppSettingsInConsoleApp/Program.cs
@@ -2,15 +2,19 @@
 
 var builder = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false);
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+    .AddUserSecrets<Program>();
 
 var configuration = builder.Build();
 
-Console.WriteLine();
+Console.WriteLine("\nSettings from appsettings.json and secrets.json:\n");
 
-Console.WriteLine($"NumericSetting: {configuration.GetValue<int>("NumericSetting")}\n");
+Console.WriteLine($" * NumericSetting:  {configuration.GetValue<int>("NumericSetting")}\n");
 
-Console.WriteLine($"TextSettingA: {configuration["SettingsGroup:SettingA"]}");
-Console.WriteLine($"TextSettingB: {configuration["SettingsGroup:SettingB"]}");
+Console.WriteLine($" * TextSettingA:  {configuration["SettingsGroup:SettingA"]}");
+Console.WriteLine($" * TextSettingB:  {configuration["SettingsGroup:SettingB"]}");
 
+Console.WriteLine($"\n * SecretSetting:  {configuration["SecretSetting"]}");
+
+Console.WriteLine("\n----");
 
